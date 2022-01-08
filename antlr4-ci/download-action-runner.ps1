13dddbe3afcd094cef183aaf68a88c2f5815d102
ERIC-WINDOWS\ericv <eric.vergnaud@wanadoo.fr>
2022-01-09 00:11:41 +0100

    A working image

---
+++ antlr4-ci/download-action-runner.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.285.1/actions-runner-win-x64-2.285.1.zip -OutFile actions-runner-win-x64-2.285.1.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.285.1.zip", "$PWD")
